6408312c9957d0345192e28429f6050c28cae11e
Neall Seth <nseth@vt.edu>
2022-05-21 18:57:15 -0400

    cleaning main

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import fs from "fs/promises";
+
 const server = express();
 server.use(express.json());
 
@@ -9,7 +10,7 @@ server.post("/create", async (req, res) => {
     await fs.writeFile(`./storage/${name}.txt`, contents);
     res.status(200).json({ res: `successfully created file: ${name}.txt` });
   } catch (err) {
-    res.status(500).json({ status: "error", msg: err });
+    res.status(500).json({ status: "error", msg: err.code });
   }
 });
 
